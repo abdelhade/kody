fce84ee1cd2131241711aa8aa8c5ce929ef959ea
Nour Ibrahim <no235795@gmail.com>
2026-02-18 17:43:23 +0200

    fix invoices includes

--- print/print_sales.php
+++ print/print_sales.php
@@ -1,4 +1,4 @@
-<?php include('../includes/header.php') ?>
+<?php include('includes/header.php') ?>
 <?php
 if (!isset($_GET['id'])) {
     echo "لا يوجد فاتورة بهذا الرقم";die;
@@ -224,4 +224,4 @@ document.addEventListener('DOMContentLoaded', function() {
 </script>
 </script>
 
-<?php include('../includes/footer.php') ?>
+<?php include('includes/footer.php') ?>
